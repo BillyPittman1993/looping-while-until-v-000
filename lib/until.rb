cd090cb915817ed7510235993a60790f16eb4caf
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-20 14:10:08 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,8 @@
 def using_until
   levitation_force = 0 
   until levitation_force == 10 
-    levitation_force += 1 
     puts "My Wingardium Leviosa is at level #{levitation_force}!"
+    levitation_force += 1
   end
 end
 
